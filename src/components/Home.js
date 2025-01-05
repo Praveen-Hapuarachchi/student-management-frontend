a8fbd8f5796f67c13acad24b18ab91f5f24aa5c9
JanithMalinda <janithmalinda8011@gmail.com>
2025-01-05 17:44:46 +0530

    calander postion change to right side and fixed it

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,21 +1,22 @@
 import React, { useState } from 'react';
-import Calendar from 'react-calendar';  // Import the Calendar component
-import 'react-calendar/dist/Calendar.css';  // Import the CSS for styling
+import Calendar from 'react-calendar'; // Import the Calendar component
+import 'react-calendar/dist/Calendar.css'; // Import the CSS for styling
 import Dashboard from './Dashboard';
 import Header from './Header';
 import Footer from './Footer';
-
+import './Home.css'; // Import the CSS file for additional styles
 
 const Home = () => {
   const [date, setDate] = useState(new Date()); // Initialize date state with current date
 
   return (
     <div>
-      <Header/>
+      <Header />
       <Dashboard />
-      <Calendar onChange={setDate} value={date} />
-      <Footer/>
-      
+      <div className="calendar-container">
+        <Calendar onChange={setDate} value={date} />
+      </div>
+      <Footer />
     </div>
   );
 };
